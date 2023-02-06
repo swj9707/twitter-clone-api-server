ed2e3e1b81fc1d37ab3365ddf8ac346e5a578783
swj9707 <swj1265@naver.com>
2023-02-06 15:20:05 +0900

    !HOTFIX : 이미지 데이터 조인 실패 버그수정 => DTO 객체 처리 실수

--- src/main/kotlin/com/swj9707/twittercloneapiserver/constant/dto/ImageDTO.kt
+++ src/main/kotlin/com/swj9707/twittercloneapiserver/constant/dto/ImageDTO.kt
@@ -1,6 +1,18 @@
 package com.swj9707.twittercloneapiserver.constant.dto
 
+import com.swj9707.twittercloneapiserver.constant.entity.Image
+
 data class ImageDTO (
     val imageId : Long?,
     var imageUrl : String
-)
+) {
+    companion object {
+        fun entityToDTO(entity : Image) : ImageDTO {
+            return ImageDTO(imageId = entity.imageId, imageUrl = entity.imageUrl)
+        }
+
+        fun entitysToListDTO(list : MutableList<Image>?) : MutableList<ImageDTO>? {
+            return list?.map{entityToDTO(it)}?.toMutableList()
+        }
+    }
+}
