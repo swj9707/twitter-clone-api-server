cf56dbe2efd714b4f3e77800d5b24d506700a53b
swj9707 <swj1265@naver.com>
2023-03-14 18:57:47 +0900

    chore : 에러 발생 시 로그 처리

--- src/main/kotlin/com/swj9707/twittercloneapiserver/global/config/security/CustomAuthenticationEntryPoint.kt
+++ src/main/kotlin/com/swj9707/twittercloneapiserver/global/config/security/CustomAuthenticationEntryPoint.kt
@@ -6,6 +6,7 @@ import com.swj9707.twittercloneapiserver.global.common.enum.ResCode
 import jakarta.servlet.ServletException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.core.AuthenticationException
@@ -17,33 +18,44 @@ import kotlin.jvm.Throws
 @Component
 class CustomAuthenticationEntryPoint : AuthenticationEntryPoint {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     @Throws(IOException::class, ServletException::class)
     override fun commence(
         request: HttpServletRequest,
         response: HttpServletResponse,
         authException: AuthenticationException
     ) {
+
+        logger.error("UNAUTHORIZED : " + request.getAttribute("exception").toString())
+
         when (request.getAttribute("exception").toString()) {
             ResCode.TOKEN_INVALID_SIGNATURE.errorCode -> {
                 setErrorResponse(response, ResCode.TOKEN_INVALID_SIGNATURE)
             }
+
             ResCode.TOKEN_MALFORMED.errorCode -> {
                 setErrorResponse(response, ResCode.TOKEN_MALFORMED)
             }
+
             ResCode.TOKEN_UNSUPPORTED.errorCode -> {
                 setErrorResponse(response, ResCode.TOKEN_UNSUPPORTED)
             }
+
             ResCode.TOKEN_ILLEGAL_ARGUMENT.errorCode -> {
                 setErrorResponse(response, ResCode.TOKEN_ILLEGAL_ARGUMENT)
             }
+
             ResCode.EXPIRED_TOKEN.errorCode -> {
                 setErrorResponse(response, ResCode.EXPIRED_TOKEN)
             }
+
             else -> {
                 setErrorResponse(response, ResCode.UNAUTHORIZED)
             }
         }
     }
+
     private fun setErrorResponse(
         res: HttpServletResponse,
         tokenStatus: ResCode
